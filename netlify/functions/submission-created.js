756167ba22ed75daca162c765dad3c25b171902c
Phil Hawksworth <phil@netlify.com>
2023-01-10 18:56:13 +0000

    event-based emails

---
+++ netlify/functions/submission-created.js
@@ -0,0 +1,20 @@
+import { sendEmail } from "@netlify/emails";
+
+exports.handler = async (event, context) => {
+
+  await sendEmail({
+    from: "phawksworth@gmail.com",
+    to: "phil@netlify.com",
+    subject: "Testing the email integration",
+    template: "subscribed",
+    parameters: {
+      name: "Phil"
+    },
+  });
+
+  return {
+    statusCode: 200,
+    body: "Hello, World... an email is on it's way."
+  };
+};
+
--- netlify/functions/subscribe.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event, context) => {
-
-  return {
-    statusCode: 200,
-    body: "Hello, World... an email is on it's way."
-  };
-};
-
